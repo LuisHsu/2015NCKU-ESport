fb106b18c6fe5e9c12c976acbb447c571ec291d8
LuisHsu <luis8404272@gmail.com>
2015-03-08 18:59:49 +0800

    1st Frame Done Index and intro frame

---
+++ index.js
@@ -0,0 +1,34 @@
+function uther_mouseover(){
+	document.getElementById("uther_button").src="Resources/uther_hover_index.png";
+}
+function uther_mouseout(){
+	document.getElementById("uther_button").src="Resources/uther_button_index.png";
+}
+
+function raxar_mouseover(){
+	document.getElementById("raxar_button").src="Resources/raxar_hover_index.png";
+}
+function raxar_mouseout(){
+	document.getElementById("raxar_button").src="Resources/raxar_button_index.png";
+}
+
+function garrosh_mouseover(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_hover_index.png";
+}
+function garrosh_mouseout(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_button_index.png";
+}
+
+function gultan_mouseover(){
+	document.getElementById("gultan_button").src="Resources/gultan_hover_index.png";
+}
+function gultan_mouseout(){
+	document.getElementById("gultan_button").src="Resources/gultan_button_index.png";
+}
+
+function malfurion_mouseover(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_hover_index.png";
+}
+function malfurion_mouseout(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_button_index.png";
+}
