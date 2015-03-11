efe9834fa8c4bb10d2e2da7d32d45f0eadd7116d
LuisHsu <luis8404272@gmail.com>
2015-03-11 14:40:20 +0800

    Intro Done

---
+++ intro.js
@@ -0,0 +1,83 @@
+function raxar_mouseover(){
+	document.getElementById("raxar_button").src="Resources/raxar_hover_index.png";
+}
+function raxar_mouseout(){
+	document.getElementById("raxar_button").src="Resources/raxar_button_index.png";
+}
+/*
+function garrosh_mouseover(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_hover_index.png";
+}
+function garrosh_mouseout(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_button_index.png";
+}
+
+function gultan_mouseover(){
+	document.getElementById("gultan_button").src="Resources/gultan_hover_index.png";
+}
+function gultan_mouseout(){
+	document.getElementById("gultan_button").src="Resources/gultan_button_index.png";
+}
+*/
+function malfurion_mouseover(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_hover_index.png";
+}
+function malfurion_mouseout(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_button_index.png";
+}
+
+/*** Submenu ***/
+function garen_mouseover(){
+	document.getElementById("garen_button").src="Resources/garen_intro_hover.png";
+}
+function garen_mouseout(){
+	document.getElementById("garen_button").src="Resources/garen_intro.png";
+}
+function garen_mouseclick(){
+	document.getElementById("content_iframe").src="intro_main.html";
+	document.getElementById("content_frame").style.height="1700px";
+}
+
+function ezreal_mouseover(){
+	document.getElementById("ezreal_button").src="Resources/ezreal_intro_hover.png";
+}
+function ezreal_mouseout(){
+	document.getElementById("ezreal_button").src="Resources/ezreal_intro.png";
+}
+function ezreal_mouseclick(){
+	document.getElementById("content_iframe").src="intro_LoL.html";
+	document.getElementById("content_frame").style.height="1500px";
+}
+
+function hearthstone_mouseover(){
+	document.getElementById("hearthstone_button").src="Resources/hearthstone_intro_hover.png";
+}
+function hearthstone_mouseout(){
+	document.getElementById("hearthstone_button").src="Resources/hearthstone_intro.png";
+}
+function hearthstone_mouseclick(){
+	document.getElementById("content_iframe").src="intro_hearth.html";
+	document.getElementById("content_frame").style.height="1300px";
+}
+
+function sona_mouseover(){
+	document.getElementById("sona_button").src="Resources/sona_intro_hover.png";
+}
+function sona_mouseout(){
+	document.getElementById("sona_button").src="Resources/sona_intro.png";
+}
+function sona_mouseclick(){
+	document.getElementById("content_iframe").src="intro_report.html";
+	document.getElementById("content_frame").style.height="600px";
+}
+
+function yasuo_mouseover(){
+	document.getElementById("yasuo_button").src="Resources/yasuo_intro_hover.png";
+}
+function yasuo_mouseout(){
+	document.getElementById("yasuo_button").src="Resources/yasuo_intro.png";
+}
+function yasuo_mouseclick(){
+	document.getElementById("content_iframe").src="intro_info.html";
+	document.getElementById("content_frame").style.height="1000px";
+}
