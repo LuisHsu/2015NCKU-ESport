b0d6b15603bdc13bfb3b4af5c877a264179140fa
LuisHsu <luis8404272@gmail.com>
2015-03-12 00:51:17 +0800

    First version done

---
+++ apply.js
@@ -0,0 +1,63 @@
+function uther_mouseover(){
+	document.getElementById("uther_button").src="Resources/uther_hover_index.png";
+}
+function uther_mouseout(){
+	document.getElementById("uther_button").src="Resources/uther_button_index.png";
+}
+/*
+function garrosh_mouseover(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_hover_index.png";
+}
+function garrosh_mouseout(){
+	document.getElementById("garrosh_button").src="Resources/garrosh_button_index.png";
+}
+
+function gultan_mouseover(){
+	document.getElementById("gultan_button").src="Resources/gultan_hover_index.png";
+}
+function gultan_mouseout(){
+	document.getElementById("gultan_button").src="Resources/gultan_button_index.png";
+}
+*/
+function malfurion_mouseover(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_hover_index.png";
+}
+function malfurion_mouseout(){
+	document.getElementById("malfurion_button").src="Resources/malfurion_button_index.png";
+}
+
+/*** Submenu ***/
+
+function ezreal_mouseover(){
+	document.getElementById("ezreal_button").src="Resources/ezreal_apply_hover.png";
+}
+function ezreal_mouseout(){
+	document.getElementById("ezreal_button").src="Resources/ezreal_apply.png";
+}
+function ezreal_mouseclick(){
+	document.getElementById("content_iframe").src="https://docs.google.com/forms/d/1eS3NvYN7XXwvZ3YLuFFYSkADnux2sAaLZLXt_lQSmjU/viewform?embedded=true";
+	document.getElementById("content_frame").style.height="850px";
+}
+
+function hearthstone_mouseover(){
+	document.getElementById("hearthstone_button").src="Resources/hearthstone_apply_hover.png";
+}
+function hearthstone_mouseout(){
+	document.getElementById("hearthstone_button").src="Resources/hearthstone_apply.png";
+}
+function hearthstone_mouseclick(){
+	document.getElementById("content_iframe").src="https://docs.google.com/forms/d/1Oq0MKm5CsG6l3cmFQxcy2RN6RV4OrdNRhRcMCYvqLEQ/viewform?embedded=true";
+	document.getElementById("content_frame").style.height="750px";
+	document.getElementById("content_iframe").height="750";
+}
+
+function sona_mouseover(){
+	document.getElementById("sona_button").src="Resources/sona_apply_hover.png";
+}
+function sona_mouseout(){
+	document.getElementById("sona_button").src="Resources/sona_apply.png";
+}
+function sona_mouseclick(){
+	document.getElementById("content_iframe").src="intro_report.html";
+	document.getElementById("content_frame").style.height="600px";
+}
